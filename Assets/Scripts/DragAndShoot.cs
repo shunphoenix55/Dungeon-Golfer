172e0d36b2e7ec5dc22e88bf5c29b19053fffbe4
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2022-12-10 18:05:34 +0530

    Fix DragAndShoot
    Previous fix unintendedly caused the negative vector to be the negative of the world coordinates instead of adding the negative to the object's coordinate

--- Assets/Scripts/DragAndShoot.cs
+++ Assets/Scripts/DragAndShoot.cs
@@ -36,13 +36,13 @@ public class DragAndShoot : MonoBehaviour
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
 
         Physics.Raycast(cameraRay, out RaycastHit hit);
-        directionIndicator.transform.position = -hit.point;//so that the pointer is in the opposite direction
+        directionIndicator.transform.position = mousePressDownPos - (hit.point - mousePressDownPos);//so that the pointer is in the opposite direction
         
     }
 
     private void OnMouseUp()
     {
-        mouseReleasePos = -directionIndicator.transform.position;//changing the direction again so that correct mouse position is calculated
+        mouseReleasePos = directionIndicator.transform.position;//changing the direction again so that correct mouse position is calculated
         Shoot(mouseReleasePos - mousePressDownPos);
         directionIndicator.SetActive(false);
     }
